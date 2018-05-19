6e5b5fb973022f5551cba6247359eedaadc3bd34
Ethan Entwitle <ethanentwistle@gmail.com>
2018-05-19 15:20:30 -0700

    UI
    Added the Basic UI

--- TestTaker/TestTaker/Source.cpp
+++ TestTaker/TestTaker/Source.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	string Question1 = "Default",Question2 = "Default",Question3 = "Default",Question4 = "Default",Question5 = "Default";
+	
+	string Input;
 
-	return 0;
+	cout << "HI Welcome to the C++ Quiz\nIf you Whuld like to take the Quiz Press Y\nIf not Press N\n"; // outputs hellow world :b
+	cin >> Input;
+	if (Input == "N" || Input == "n")
+	{
+		return 0;
+	}
+	else if(Input == "Y" || Input == "y")
+	{
+		
+	}
+	cin >> Input;
 }
+
+void Question1()
+{
+//	cout << 
+	cout << "Your choices are\nA: Happy\nB: Lappy\n"; // The Question
+}
+
